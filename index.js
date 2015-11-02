fdd54d25930a781a4d8a54184d0daf2ee00deeab
Josh <joshpaulchan@gmail.com>
2015-11-02 14:47:38 -0500

    Organize Routes
    Reorganized the routes for index.js to separate views from api. Added some
    boilerplate for Jaya.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var multer = require('multer');
 var app = express();
 var upload = multer({ dest: './uploads/'});
 
-app.use(express.static('assets'));
+app.use(express.static(__dirname + '/assets'));
 
 app.use(multer({
 	dest: './uploads/',
@@ -23,17 +23,23 @@ app.use(multer({
 	}
 }));
 
-// Route root
+// VIEWS
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/upload/', function(req, res) {
+	res.sendFile(__dirname + '/assets/upload.html');
+})
 
 app.get('/gallery', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-// Upload route
-app.post('/api/upload', function(req, res) {
+// API
+var API_ROOT = '/api';
+
+app.post(API_ROOT + '/upload', function(req, res) {
 	upload(req, res, function(err) {
 		if (err) {
 			return res.end("Error uploading the file.");
@@ -42,6 +48,14 @@ app.post('/api/upload', function(req, res) {
 	});
 });
 
+api.get(API_ROOT + '/images', function(req, res) {
+	// TODO:
+	// Send JSON response with array of images with names, thumbnails and links that need to be copied
+	console.log('Fetching image info..');
+})
+
+
+// MAIN
 app.listen(3000, function() {
 	console.log("Listening on *:3000");
 });
